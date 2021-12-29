a09e1b14c5481fbcf8d711ce044daf4c2a77120d
Nzzz964 <56777838+Nzzz964@users.noreply.github.com>
2021-12-29 14:57:12 +0800

    fix: prompt incorrect

--- ddnsv6.sh
+++ ddnsv6.sh
@@ -15,7 +15,7 @@ if [ -z "$v6addr" ]; then
     echo "v6addr cannot empty" >&2
 fi
 
-echo "ipv4: ${v6addr}"
+echo "ipv6: ${v6addr}"
 
 domain_id=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${sub_domain}" \
     -H "X-Auth-Email: ${email}" \
